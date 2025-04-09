642a927493de9e25d1b57b79b88a4fb359da6023
jaivema <jaivema@hotmail.com>
2025-04-09 12:12:44 +0200

    shopping cart chapter

---
+++ src/components/Guitar.jsx
@@ -0,0 +1,25 @@
+const Guitar = ({ guitar, addToCart }) => {
+
+    const { image, name, description, price } = guitar
+
+    return (
+        <article className="col-md-6 col-lg-4 my-4 row align-items-center">
+            <div className="col-4">
+                <img className="img-fluid" src={`/img/${image}.jpg`} alt={name} />
+            </div>
+            <div className="col-8">
+                <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+                <p>{description}</p>
+                <p className="fw-bold fs-3 text-primary">€{price}</p>
+                <button
+                    type="button"
+                    className="btn btn-dark d-block w-100"
+                    onClick={() => addToCart(guitar)}
+                >
+                    Añadir al carrito
+                </button>
+            </div>
+        </article>
+    )
+}
+export default Guitar
---
+++ src/components/Header.jsx
@@ -0,0 +1,85 @@
+import { useMemo } from "react"
+
+export default function Header({ cart, clearCart, removeFromCart, decreaseQuantity, increaseQuantity }) {
+    const isEmpty = useMemo(() => cart.length === 0, [cart])
+    const cartTotal = useMemo( () => cart.reduce( (total, item ) => total + (item.quantity * item.price), 0), [cart] )
+    
+    return (
+        <header className="py-5 header">
+            <div className="container-xl">
+                <div className="row justify-cotent-center justify-content-md-between">
+                    <div className="col-8 col-md-3 ">
+                        <a href="index.html">
+                            <img className="img-fluid header-guitarra" src="/img/logo.svg" alt="imagen logo" />
+                        </a>
+                    </div>
+                    <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+                        <div className="carrito">
+                            <img className="img-fluid" src="/img/carrito.png" alt="carrito" />
+                            <div id="carrito" className="bg-white p-3">
+                                {isEmpty ? (
+                                    <p className="text-center fw-bold">El carrito está vacío</p>
+                                ) : (<>
+                                    <p className="d-inline-flex">hay {cart.length} productos</p>
+                                    <table className="w-100 table">
+                                        <thead>
+                                            <tr>
+                                                <th>Imagen</th>
+                                                <th>Nombre</th>
+                                                <th>Precio</th>
+                                                <th>Cantidad</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {cart.map(guitar => (
+                                                <tr key={guitar.id}>
+                                                    <td>
+                                                        <img className="img-fluid" src={`/img/${guitar.image}.jpg`} alt={guitar.name} />
+                                                    </td>
+                                                    <td>{guitar.name}</td>
+                                                    <td>€{guitar.price}</td>
+                                                    <td>
+                                                        <button
+                                                            type="button"
+                                                            className="btn btn-dark"
+                                                            onClick={() => decreaseQuantity(guitar.id)}
+                                                        >
+                                                            -
+                                                        </button>
+                                                            {guitar.quantity}
+                                                        <button
+                                                            type="button"
+                                                            className="btn btn-dark"
+                                                            onClick={() => increaseQuantity(guitar.id)}
+                                                        >
+                                                            +
+                                                        </button>
+                                                    </td>
+                                                    <td>
+                                                        <button
+                                                            className="btn btn-danger"
+                                                            type="button"
+                                                            onClick={() => removeFromCart(guitar.id)}
+                                                        >
+                                                            X
+                                                        </button>
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                    <p className="text-end">Total: <span className="fw-bold">€{cartTotal}</span></p>
+                                    <button className="btn btn-dark w-100 mt-3 p-2"
+                                        onClick={() => clearCart()}
+                                    >
+                                        Vaciar carrito
+                                    </button>
+                                </>)}
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    )
+}
